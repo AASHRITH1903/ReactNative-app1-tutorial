d53a311bb9e3368dc02ac647c7780e137c5e1428
AASHRITH1903 <cs19b025@iittp.ac.in>
2021-01-03 18:00:57 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "red",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
